d867fb571944b6d402571400ec8d33f73c061a5f
Timur Gataullin <tiga1115@netcracker.com>
2016-07-25 17:33:01 +0300

    1st commit. use https://github.com/Java-Help/ViewPagerIndicator library

---
+++ app/src/main/java/com/friendevent/vpisample/SampleTitlesCenterClickListener.java
@@ -0,0 +1,33 @@
+package com.friendevent.vpisample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.widget.Toast;
+
+import com.friendevent.vpisample.viewpagerindicator.TitlePageIndicator;
+import com.friendevent.vpisample.viewpagerindicator.TitlePageIndicator.IndicatorStyle;
+import com.friendevent.vpisample.viewpagerindicator.TitlePageIndicator.OnCenterItemClickListener;
+
+public class SampleTitlesCenterClickListener extends BaseSampleActivity implements OnCenterItemClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_titles);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        indicator.setFooterIndicatorStyle(IndicatorStyle.Underline);
+        indicator.setOnCenterItemClickListener(this);
+        mIndicator = indicator;
+    }
+
+    @Override
+    public void onCenterItemClick(int position) {
+        Toast.makeText(this, "You clicked the center title!", Toast.LENGTH_SHORT).show();
+    }
+}
